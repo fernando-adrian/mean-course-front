c08c6b8cf3bf7dfc07e2b23f3e886721eaa57718
fernando robles <advancexz@gmail.com>
2022-06-23 09:22:34 -0700

    posts service added, Input and Output removed

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -1,18 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
   // posts = [
   //   { title: 'First post', content: 'This is the first' },
   //   { title: 'Second post', content: 'This is the second' },
   //   { title: 'Third post', content: 'This is the third' },
   // ];
-  @Input()
-  posts:Post[]=[];
+  posts: Post[] = [];
+  private postsSub: Subscription = Subscription.EMPTY;
 
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe({
+      next: (posts: Post[]) => (this.posts = posts),
+      error: () => console.log('error'),
+      complete: () => console.log('complete'),
+    });
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
+  }
 }
