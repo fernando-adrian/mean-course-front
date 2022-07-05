3f4745432dce4907c97511439a8da29a7e7dc759
fernando robles <advancexz@gmail.com>
2022-07-05 12:27:48 -0700

    authguard implemented for features that requires to be authenticated, autoauth implemented, session expiration duration implemented

---
+++ mean-course/src/app/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    const isAuthenticated = this.authService.getIsAuthenticated();
+    if (!isAuthenticated) {
+      this.router.navigate(['/login']);
+    }
+    return isAuthenticated;
+  }
+}
